83b4d457ce10de6ad0c67c0a0d79b845dec03252
Matt Baier <mbaier06@gmail.com>
2021-02-13 13:34:19 -0600

    included bonus 1 solution with re-writing to function for instant rendering

--- lab/kelloggram.js
+++ lab/kelloggram.js
@@ -1,9 +1,53 @@
 let db = firebase.firestore()
 console.log(db)
 
-// async function renderPost(post) {
+async function renderPost(postInputID, postUsername, postImageUrl, postNumberOfLikes) {
+  document.querySelector('.posts').insertAdjacentHTML('beforeend', `
+  <div class="post-${postInputID} md:mt-16 mt-8 space-y-8">
+    <div class="md:mx-0 mx-4">
+      <span class="font-bold text-xl">${postUsername}</span>
+    </div>
 
-// }
+    <div>
+      <img src="${postImageUrl}" class="w-full">
+    </div>
+
+    <div class="text-3xl md:mx-0 mx-4">
+      <button class="like-button">❤️</button>
+      <span class="likes">${postNumberOfLikes}</span>
+    </div>
+  </div>
+  `)
+// Step 3: Implement the "like" button
+// - In the code we wrote for Step 2, attach an event listener to
+//   every "like-button".
+// - HINT: simply doing document.querySelector('.like-button').addEventListener(...)
+//   is not enough. We must uniquely identify each like button by changing 
+//   the class name or adding another class name to the parent.
+// - Ensure that each like button click is unique and has the post ID attached by
+//   writing out the post ID to the JavaScript console, e.g. 
+//   "post abcdefgwxyz like button clicked!"
+  let likeButton = document.querySelector(`.post-${postInputID} .like-button`)
+  likeButton.addEventListener('click', async function(event){
+  event.preventDefault()
+  console.log(` post ${postInputID} was clicked`)
+
+  // - Increment the number of likes for each post in the HTML:
+  // Get the existing number of likes from the "likes" element
+  let existingNumberOfLikes = document.querySelector(`.post-${postInputID} .likes`).innerHTML
+  // console.log(existingNumberOfLikes)
+  // Increment the number of likes (converting to an Integer first) by 1
+  let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+  document.querySelector(`.post-${postInputID} .likes`).innerHTML = newNumberOfLikes
+  // - Increment the number of likes for the given post in Firestore,
+  //   using .update() – see "Common Use-Cases: Timestamp and Incrementing Values"
+  //   in the reference.
+
+    await db.collection('posts').doc(postInputID).update({
+      likes: firebase.firestore.FieldValue.increment(1)
+    })
+  })
+}
 
 window.addEventListener('DOMContentLoaded', async function(event) {
   // Step 1: Accept input from the "new post" form and write 
@@ -25,12 +69,15 @@ window.addEventListener('DOMContentLoaded', async function(event) {
 
   let postUsername = document.querySelector('#username').value 
   let postImageUrl= document.querySelector('#image-url').value
+  let postNumberOfLikes = 0
   let docRef = await db.collection('posts').add({ 
     username: postUsername, 
     imageUrl: postImageUrl, 
-    likes: 0 
+    likes: postNumberOfLikes
   })
-  // console.log(docRef)
+  let postInputID = docRef.id
+  // use the new function which replaces the repetitive insertAdjacentHTML and event listener code
+  renderPost(postInputID, postUsername, postImageUrl, postNumberOfLikes)
   })
   
   // Step 2: Read existing posts from Firestore and display them
@@ -52,57 +99,14 @@ window.addEventListener('DOMContentLoaded', async function(event) {
     console.log(postUsername)
     let postImageUrl = postInputData.imageURL
     console.log(postImageUrl)
+    let postNumberOfLikes = postInputData.likes
   // - Inside the loop, using insertAdjacentHTML, add posts
   //   to the page inside the provided "posts" div; sample code
   //   provided:
   //
-    document.querySelector('.posts').insertAdjacentHTML('beforeend', `
-      <div class="post-${postInputID} md:mt-16 mt-8 space-y-8">
-        <div class="md:mx-0 mx-4">
-          <span class="font-bold text-xl">${postUsername}</span>
-        </div>
-  
-        <div>
-          <img src="${postImageUrl}" class="w-full">
-        </div>
-   
-        <div class="text-3xl md:mx-0 mx-4">
-          <button class="like-button">❤️</button>
-          <span class="likes">${postInputData.likes}</span>
-        </div>
-      </div>
-    `)
-    // Step 3: Implement the "like" button
-  // - In the code we wrote for Step 2, attach an event listener to
-  //   every "like-button".
-  // - HINT: simply doing document.querySelector('.like-button').addEventListener(...)
-  //   is not enough. We must uniquely identify each like button by changing 
-  //   the class name or adding another class name to the parent.
-  // - Ensure that each like button click is unique and has the post ID attached by
-  //   writing out the post ID to the JavaScript console, e.g. 
-  //   "post abcdefgwxyz like button clicked!"
-  let likeButton = document.querySelector(`.post-${postInputID} .like-button`)
-  likeButton.addEventListener('click', async function(event){
-    event.preventDefault()
-    console.log(` post ${postInputID} was clicked`)
- 
-  // - Increment the number of likes for each post in the HTML:
-  // Get the existing number of likes from the "likes" element
-  let existingNumberOfLikes = document.querySelector(`.post-${postInputID} .likes`).innerHTML
-  // console.log(existingNumberOfLikes)
-  // Increment the number of likes (converting to an Integer first) by 1
-  let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
-  document.querySelector(`.post-${postInputID} .likes`).innerHTML = newNumberOfLikes
-  // - Increment the number of likes for the given post in Firestore,
-  //   using .update() – see "Common Use-Cases: Timestamp and Incrementing Values"
-  //   in the reference.
-  
-    await db.collection('posts').doc(postInputID).update({
-      likes: firebase.firestore.FieldValue.increment(1)
-    })
-  })
+  renderPost(postInputID, postUsername, postImageUrl, postNumberOfLikes)
 }
-  
+
 
   // Bonus:
   // - Add a new post and notice how it doesn't update the page until
